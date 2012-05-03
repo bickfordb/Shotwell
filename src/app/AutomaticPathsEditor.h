d365feb8c979b4afe556e5369b99b63a864ae922
Brandon Bickford <bickfordb@gmail.com>
2012-05-03 09:55:48 -0700

    Forgot these

---
+++ src/app/AutomaticPathsEditor.h
@@ -0,0 +1,15 @@
+#import <Cocoa/Cocoa.h>
+#import "app/TableViewController.h"
+#import "app/LocalLibrary.h"
+
+@interface AutomaticPathsEditor : ViewController {
+  TableViewController *table_; 
+  LocalLibrary *localLibrary_;
+}
+@property (retain) TableViewController *table;
+@property (retain) LocalLibrary *localLibrary;
+
+- (id)initWithLocalLibrary:(LocalLibrary *)localLibrary;
+@end
+// vim: filetype=objcpp
+
